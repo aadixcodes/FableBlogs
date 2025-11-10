a37d0fa17132de1463b967e5e164ae7095e7bd73
Aditya Patel <23100BTCSE14732@svvvindore.onmicrosoft.com>
2025-11-11 01:16:21 +0530

    pushed

---
+++ src/app/news-articles/[slug]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsArticlesSlug = () => {
+  return (
+    <div>
+      <h1>News Articles Slug</h1>
+    </div>
+  )
+}
+
+export default NewsArticlesSlug
---
+++ src/app/news-articles/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsArticles = () => {
+  return (
+    <div>
+      <h1>News Articles</h1>
+    </div>
+  )
+}
+
+export default NewsArticles
